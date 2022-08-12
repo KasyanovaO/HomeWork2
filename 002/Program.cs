b39c8ebb9848cd20c7be11de5c12da80ccf5cb9f
OK <@mail.ru>
2022-08-12 19:00:41 +0300

    add 002

---
+++ 002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число ");
+        string numbers = Console.ReadLine();
+        Console.WriteLine("третья цифра этого числа = ");
+        Console.WriteLine($"{numbers[2]}");
+    }
+}
